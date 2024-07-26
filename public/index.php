e64116a5b4c50d2eacfab97e9eeb8320dc325e49
Nauman Bashir <nauman.bashir@hotmail.com>
2024-07-27 03:11:02 +0500

    Added Request and Response Class

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Panda\Application;
 
+$request = \Panda\Http\Request::createFromGlobals();
+
+dd($request);
+
 $rootDir = dirname(__DIR__);
 $app = new Application($rootDir);
 
