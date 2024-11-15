3b954fb3f2e52c4844cd0b66061630260b9729d3
Nauman Bashir <nauman.bashir@hotmail.com>
2024-11-15 22:42:45 +0500

    Added php unit and tested assertion

---
+++ xplore/tests/ContainerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Xplore\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ContainerTest extends TestCase
+{
+    /** @test */
+    public function a_service_can_be_retrieved_from_the_container()
+    {
+        $this->assertTrue(true);
+    }
+
+}
